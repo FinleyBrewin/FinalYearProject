9309adc9b1857ceee526933de7e550f234849177
Finley Brewin <94471550+FinleyBrewin@users.noreply.github.com>
2023-07-07 18:28:24 +0100

    Delete src directory

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "src/styles/globals.css";
-import "react-datepicker/dist/react-datepicker.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
